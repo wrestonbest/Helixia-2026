c8b81133024c98b7bb2776e6c6df6c8f78ff0bab
wrestonbest <wreston7@hotmail.com>
2026-02-14 13:40:56 +0300

    Update script to load V3 instead of V2

--- Universal Reach/v1.lua
+++ Universal Reach/v1.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/wrestonbest/Helixia-2026/refs/heads/main/Universal%20Reach/V2.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/wrestonbest/Helixia-2026/refs/heads/main/Universal%20Reach/V3.lua"))()
